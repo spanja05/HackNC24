4536e3669dd79e374b9da454bf330afc2fa4c65f
Simone Panja <simone.panja@gmail.com>
2024-11-02 17:10:55 -0400

    [Implementation] Driver class

--- HackNC/src/Driver.java
+++ HackNC/src/Driver.java
@@ -1,93 +1,16 @@
 
+import java.util.Scanner;
+
 public class Driver {
-    public static void main (String [] args) {
 
-        login();
+    private static User user;
 
-        Scanner scnr = new Scanner(System.in);
-
-        System.out.print("What size do you want?");
-        String size = scnr.next();
-
-        switch (size.toLowerCase()) {
-            case "small":
-                System.out.println("Person doesn't have a weapon")
-                break;
-            case "medium":
-                System.out.println("Person has a weapon")
-                break;
-            case "large":
-                System.out.println("I do not know if the person has a weapon")
-                break;                 
-            default:
-                System.out.println("Invalid size selction");
-                System.exit(1);
-        }
-
-        System.out.print("How many slices do you want? Options are 1-4");
-        int slices = scnr.nextInt();
-
-        switch (slices) {
-            case 1:
-                System.out.println("Danger level 1-2")
-                break;
-            case 2:
-                System.out.println("Danger level 2-5")
-                break;
-            case 3:
-                System.out.println("Danger Level 6-8")
-                break;  
-            case 4:
-                System.out.println("Danger level 8-10")
-                break;                 
-            default:
-                System.out.println("Invalid slizes selection");
-                System.exit(1);
-        }
-
-        System.out.print("Would you like your order to be delivered. Respond with yes or no");
-        String deliver = scnr.next();
-
-        switch (deliver) {
-            case "yes":
-                System.out.println("In a crowded area")
-                break;
-            case "no":
-                System.out.println("Not in a crowded area")
-                break;                 
-            default:
-                System.out.println("Invalid option");
-                System.exit(1);
-        }
-
-        System.out.print("Would flavor would you like?");
-        System.out.print("The options are vanilla, chocolate, strawberry, oreo, coffee");
-        String flavor = scnr.next();
-
-        switch (flavor) {
-            case "vanilla":
-                System.out.println("Sexual")
-                break;
-            case "chocolate":
-                System.out.println("Physical")
-                break;
-            case "strawberry":
-                System.out.println("Mental")
-                break;  
-            case "oreo":
-                System.out.println("Gerneral Fear")
-                break; 
-            case "coffee":
-                System.out.println("Gerneral Fear")
-                break;                 
-            default:
-                System.out.println("Invalid slizes selection");
-                System.exit(1);
-        }
-
+    public static void main(String[] args) {
+        user = createAccount();
+        chooseInterface();
     }
 
-    public void login() {
+    public static User createAccount() {
 
         Scanner scnr1 = new Scanner(System.in);
 
@@ -101,20 +24,187 @@ public class Driver {
         String email = scnr1.next();
 
         System.out.print("Is your Location Enabled: ");
-        boolean locationEnabled = scnr1.nextBoolean();
-
-        System.out.print("What food do you want: ");
-        String foodOption = scnr1.next();
+        String userResponse1 = scnr1.next();
+        boolean locationEnabled;
+        if (userResponse1.toLowerCase().equals("yes")) {
+            locationEnabled = true;
+        } else {
+            locationEnabled = false;
+        }
 
         System.out.print("Location: ");
         String location = scnr1.next();
 
         System.out.print("Are you a volunteer?: ");
-        boolean isVolunteer = scnr1.nextBoolean();
-
-        User user = new User(name, phoneNumber, email, locationEnabled, foodOption, location, isVolunteer);
-
+        String userResponse2 = scnr1.next();
+        boolean isVolunteer;
+        if (userResponse2.toLowerCase().equals("yes")) {
+            isVolunteer = true;
+        } else {
+            isVolunteer = false;
+        }
         scnr1.close();
 
+        User user = new User(name, phoneNumber, email, locationEnabled, location, isVolunteer);
+
+        return user;
+    }
+
+    public static void chooseInterface() {
+        Scanner scnr2 = new Scanner(System.in);
+        System.out.print("What food do you want: ");
+        String foodOption = scnr2.next();
+
+        if (foodOption.equalsIgnoreCase("pizza")) {
+            Food foodChoice = new Pizza();
+
+            if (foodChoice.getDangerSituation().equals("Sexual")) {
+                System.out.println("" + user.getName() + " is in a sexual danger situation");
+                if (foodChoice.getHasWeapon()) {
+                    System.out.println(user.getName() + "'s attacker has weapons");
+                    if (foodChoice.getDangerLevel() == 1) {
+                        System.out.println(user.getName() + " is in danger level 1");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 2) {
+                        System.out.println(user.getName() + " is in danger level 2");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 3) {
+                        System.out.println(user.getName() + " is in danger level 3");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 4) {
+                        System.out.println(user.getName() + " is in danger level 4");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    }
+                }
+            }
+
+            if (foodChoice.getDangerSituation().equals("Physical")) {
+                System.out.println("" + user.getName() + " is in a physical danger situation");
+                if (foodChoice.getHasWeapon()) {
+                    System.out.println(user.getName() + "'s attacker has weapons");
+                    if (foodChoice.getDangerLevel() == 1) {
+                        System.out.println(user.getName() + " is in danger level 1");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 2) {
+                        System.out.println(user.getName() + " is in danger level 2");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 3) {
+                        System.out.println(user.getName() + " is in danger level 3");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 4) {
+                        System.out.println(user.getName() + " is in danger level 4");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    }
+                }
+            }
+
+            if (foodChoice.getDangerSituation().equals("Mental")) {
+                System.out.println("" + user.getName() + " is in a mental danger situation");
+                if (foodChoice.getHasWeapon()) {
+                    System.out.println(user.getName() + "'s attacker has weapons");
+                    if (foodChoice.getDangerLevel() == 1) {
+                        System.out.println(user.getName() + " is in danger level 1");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 2) {
+                        System.out.println(user.getName() + " is in danger level 2");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 3) {
+                        System.out.println(user.getName() + " is in danger level 3");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 4) {
+                        System.out.println(user.getName() + " is in danger level 4");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    }
+                }
+            }
+
+            if (foodChoice.getDangerSituation().equals("General Fear")) {
+                System.out.println("" + user.getName() + " is in a general fear danger situation");
+                if (foodChoice.getHasWeapon()) {
+                    System.out.println(user.getName() + "'s attacker has weapons");
+                    if (foodChoice.getDangerLevel() == 1) {
+                        System.out.println(user.getName() + " is in danger level 1");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 2) {
+                        System.out.println(user.getName() + " is in danger level 1");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 3) {
+                        System.out.println(user.getName() + " is in danger level 1");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    } else if (foodChoice.getDangerLevel() == 4) {
+                        System.out.println(user.getName() + " is in danger level 1");
+                        if (foodChoice.isInCrowdedArea()) {
+                            System.out.println(user.getName() + " is in a crowded area");
+                        } else {
+                            System.out.println(user.getName() + " is not in a crowded area");
+                        }
+                    }
+                }
+            }
+        } else if (foodOption.equalsIgnoreCase("cake")) {
+            // To do
+        }
+
+        scnr2.close();
     }
 }
