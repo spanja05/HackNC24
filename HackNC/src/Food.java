cd4805a821beb5290910505d512a60cc2b8eb05a
askalidi <askalidi@ncsu.edu>
2024-11-02 14:07:06 -0400

    Created Food class

---
+++ HackNC/src/Food.java
@@ -0,0 +1,15 @@
+
+
+public abstract class Food {
+
+    private String name;
+    private String size;
+    private int slices;
+    private int toppings;
+    
+    public Food() {
+
+    }
+
+
+}
