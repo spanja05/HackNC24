4058c83a77dec7b770c482c00e344130e6742095
Simone Panja <simone.panja@gmail.com>
2024-11-02 17:10:10 -0400

    [Implementation] Food class

--- HackNC/src/Food.java
+++ HackNC/src/Food.java
@@ -1,18 +1,36 @@
-
-
 public abstract class Food {
 
-    private String name;
-    private boolean delivery;
-    private String size;
-    private int slices;
-    private int toppings;
-    
-    public Food() {
+    /**
+     * private boolean delivery = false;
+     * private String size = "s";
+     * private int slices = -1;
+     * private int toppings = -1;
+     */
 
+    String dangerSituation;
+    boolean hasWeapon;
+    int dangerLevel;
+    boolean inCrowdedArea;
+
+    public Food() {
     }
 
-    public abtract String deliveryResponse();
+    public abstract void deliveryResponse();
 
+    public String getDangerSituation() {
+        return this.dangerSituation;
+    }
+
+    public boolean getHasWeapon() {
+        return this.hasWeapon;
+    }
+
+    public int getDangerLevel() {
+        return this.dangerLevel;
+    }
+
+    public boolean isInCrowdedArea() {
+        return inCrowdedArea;
+    }
 
 }
